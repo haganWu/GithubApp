701005aef2e6118b005f1af65541d00cad7dd42c
WuHaiheng <10173167@qq.com>
2021-10-14 11:32:38 +0800

    修改Android端应用包名及应用图标

--- android/app/src/main/java/com/hagan/github/MainActivity.java
+++ android/app/src/main/java/com/hagan/github/MainActivity.java
@@ -1,4 +1,4 @@
-package com.githubapp;
+package com.hagan.github;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/hagan/github/MainApplication.java
+++ android/app/src/main/java/com/hagan/github/MainApplication.java
@@ -1,13 +1,15 @@
-package com.githubapp;
+package com.hagan.github;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -17,7 +19,7 @@ public class MainApplication extends Application implements ReactApplication {
       new ReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
+          return com.hagan.github.BuildConfig.DEBUG;
         }
 
         @Override
@@ -56,13 +58,13 @@ public class MainApplication extends Application implements ReactApplication {
    */
   private static void initializeFlipper(
       Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
+    if (com.hagan.github.BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.githubapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.hagan.github.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
